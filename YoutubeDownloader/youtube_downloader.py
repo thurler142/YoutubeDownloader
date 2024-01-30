fe041e1e65332ff1d7679b33be85ddf7275dbd2d
thurler142 <89205433+thurler142@users.noreply.github.com>
2024-01-30 13:26:33 -0300

    Add files via upload
    Adicionado uma interface simples

---
+++ YoutubeDownloader/youtube_downloader.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+from PySimpleGUI import PySimpleGUI as sg
+
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text('URL', auto_size_text=True), ],
+    [sg.InputText(key = 'link')],
+    [sg.Button('Baixar', auto_size_button = True)]
+]
+window = sg.Window('Minha Janela', layout)
+def YTDownload ():
+    event, values = window.read()
+    link = values['link']
+    video = YouTube(link)
+    video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+YTDownload()  
+
+while True:
+    event, values = window.read()
+    if event == 'Baixar':
+        YTDownload()
+        sg.popup('Baixado com Sucesso!')
+        
